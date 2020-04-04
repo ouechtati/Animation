4dc96c2faf11769cdbe29fe5f52ca8d8fa3b3f61
acer <acer@acer-PC>
2020-04-04 02:11:29 +0200

    Creation des entites

---
+++ angular/src/main/java/com/udemy/angular/entities/AnimeCharacter.java
@@ -0,0 +1,12 @@
+package com.udemy.angular.entities;
+
+import java.io.Serializable;
+
+public class AnimeCharacter implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ angular/src/main/java/com/udemy/angular/entities/User.java
@@ -0,0 +1,18 @@
+package com.udemy.angular.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
