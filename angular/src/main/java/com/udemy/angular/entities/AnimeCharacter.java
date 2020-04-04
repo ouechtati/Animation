e33001473530835ae15c5347218908bc09ac5c81
acer <acer@acer-PC>
2020-04-04 19:22:15 +0200

    Creation entre les tables(User&AnimeCharacter) et la BD.

--- angular/src/main/java/com/udemy/angular/entities/AnimeCharacter.java
+++ angular/src/main/java/com/udemy/angular/entities/AnimeCharacter.java
@@ -1,12 +1,115 @@
 package com.udemy.angular.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
 public class AnimeCharacter implements Serializable {
 
-	/**
+    /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Id
+    @GeneratedValue
+    private Long idCharacter;
+
+    private String characterName;
+
+    private String legend;
+
+    private String category;
+
+    private String strength;
+
+    @Lob
+    private byte[] photo;
+
+    private boolean shared;
+
+    @Transient
+    private Long idOwner;
+
+    @ManyToOne
+    @JoinColumn(name = "idUser")
+    private User user;
+
+    public AnimeCharacter() {
+    }
+
+    public Long getIdCharacter() {
+        return idCharacter;
+    }
+
+    public void setIdCharacter(Long idCharacter) {
+        this.idCharacter = idCharacter;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStrength() {
+        return strength;
+    }
+
+    public void setStrength(String strength) {
+        this.strength = strength;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public boolean isShared() {
+        return shared;
+    }
+
+    public void setShared(boolean shared) {
+        this.shared = shared;
+    }
+
+    @JsonIgnore
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getIdOwner() {
+        return idOwner;
+    }
+
+    public void setIdOwner(Long idOwner) {
+        this.idOwner = idOwner;
+    }
+
+    public String getLegend() {
+        return legend;
+    }
+
+    public void setLegend(String legend) {
+        this.legend = legend;
+    }
 }
--- angular/src/main/java/com/udemy/angular/entities/User.java
+++ angular/src/main/java/com/udemy/angular/entities/User.java
@@ -1,18 +1,103 @@
+
+	
 package com.udemy.angular.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.io.Serializable;
-
-import javax.persistence.Entity;
-
+import java.util.List;
 
 @Entity
+@Data
+@AllArgsConstructor
+@Builder
 public class User implements Serializable {
 
-	/**
+    /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
 
+	@Id
+    @GeneratedValue
+    private Long idUser;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String mail;
+
+    private String password;
+
+    @Lob
+    private byte[] photo;
+
+    @OneToMany(mappedBy = "user")
+    private List<AnimeCharacter> characters;
+
+    public User() {
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    @JsonIgnore
+    public List<AnimeCharacter> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(List<AnimeCharacter> characters) {
+        this.characters = characters;
+    }
 }
