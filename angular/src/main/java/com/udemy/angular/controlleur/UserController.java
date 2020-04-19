42e2f9dd75f6ecf975a17ea80a69084014772c24
acer <acer@acer-PC>
2020-04-19 04:37:40 +0200

    Méthode de récupération d'un user.

--- angular/src/main/java/com/udemy/angular/controlleur/UserController.java
+++ angular/src/main/java/com/udemy/angular/controlleur/UserController.java
@@ -3,9 +3,11 @@ package com.udemy.angular.controlleur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.udemy.angular.entities.User;
 import com.udemy.angular.repositories.IUser;
 
 @RestController
@@ -18,11 +20,24 @@ public class UserController {
 	
 	
 	/*liste utilisateur*/
-	
 	@GetMapping("/")
 	public ResponseEntity findAll() {
 		return ResponseEntity.ok(userRepository.findAll());
 		
+	}
+	
+	/*récupération d'un utilisateur*/
+	@GetMapping("/{idUser}")
+	public ResponseEntity findUserById (@PathVariable(name="idUser")Long idUser) {
+		if(idUser == null) {
+			return ResponseEntity.badRequest().body("cannot retrive user with null ID");
+		}
+		
+		User  user = userRepository.getOne(idUser);
+		if(user == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().body(user);
 	}
 
 }
