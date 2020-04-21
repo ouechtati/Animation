7390bf34feb83e40ab52f5addb47a2ed06b0fc29
acer <acer@acer-PC>
2020-04-21 17:01:50 +0200

    Création controlleur Anime (Api Rest)

---
+++ angular/src/main/java/com/udemy/angular/controlleur/AnimeController.java
@@ -0,0 +1,10 @@
+package com.udemy.angular.controlleur;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/anime")
+public class AnimeController {
+
+}
