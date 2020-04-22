73a4fad2dd685205d854ccb4f26be3ed5a507d58
acer <acer@acer-PC>
2020-04-22 02:04:42 +0200

    rechercher (Api Rest Anime)

--- angular/src/main/java/com/udemy/angular/controlleur/AnimeController.java
+++ angular/src/main/java/com/udemy/angular/controlleur/AnimeController.java
@@ -1,10 +1,21 @@
 package com.udemy.angular.controlleur;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.angular.repositories.IanimeCharcacter;
 
 @RestController
 @RequestMapping("/v1/anime")
 public class AnimeController {
 
+	private IanimeCharcacter animeRepository;
+	
+	/*récupération d'un user */
+	@GetMapping("/")
+	public ResponseEntity findAll() {
+		return ResponseEntity.ok(animeRepository.findAll());	
+	}
 }
