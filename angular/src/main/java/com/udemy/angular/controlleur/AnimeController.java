dc3c9489377c9a146c71ce8194782a258ff85889
acer <acer@acer-PC>
2020-04-23 15:44:14 +0200

    rechercher anime avec id (api rest anime)

--- angular/src/main/java/com/udemy/angular/controlleur/AnimeController.java
+++ angular/src/main/java/com/udemy/angular/controlleur/AnimeController.java
@@ -2,9 +2,11 @@ package com.udemy.angular.controlleur;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.udemy.angular.entities.AnimeCharacter;
 import com.udemy.angular.repositories.IanimeCharcacter;
 
 @RestController
@@ -13,9 +15,21 @@ public class AnimeController {
 
 	private IanimeCharcacter animeRepository;
 	
-	/*récupération d'un user */
+	/*rechercher d'un anime */
 	@GetMapping("/")
 	public ResponseEntity findAll() {
 		return ResponseEntity.ok(animeRepository.findAll());	
+	}
+	
+	/*rechercher anime avec id*/
+	public ResponseEntity findAnimeById(@PathVariable(name="idAnime") Long idAnime) {
+		if(idAnime == null) {
+			return ResponseEntity.badRequest().body("cannot find anime avec id null");
+		}
+		AnimeCharacter anime= animeRepository.getOne(idAnime);
+		if(anime == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(anime);
 	}
 }
