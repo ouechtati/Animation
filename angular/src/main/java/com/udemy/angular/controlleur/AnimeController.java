9cba0f5a5040dcc7e1a2d057aff5029a649e3bcd
acer <acer@10.225.182.60>
2020-05-01 05:01:14 +0200

    fixed bug & Rest Controller

--- angular/src/main/java/com/udemy/angular/controlleur/AnimeController.java
+++ angular/src/main/java/com/udemy/angular/controlleur/AnimeController.java
@@ -3,6 +3,8 @@ package com.udemy.angular.controlleur;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,23 +15,32 @@ import com.udemy.angular.repositories.IanimeCharcacter;
 @RequestMapping("/v1/anime")
 public class AnimeController {
 
-	private IanimeCharcacter animeRepository;
+	private IanimeCharcacter charcterRepository;
 	
 	/*rechercher d'un anime */
 	@GetMapping("/")
 	public ResponseEntity findAll() {
-		return ResponseEntity.ok(animeRepository.findAll());	
+		return ResponseEntity.ok(charcterRepository.findAll());	
 	}
 	
 	/*rechercher anime avec id*/
-	public ResponseEntity findAnimeById(@PathVariable(name="idAnime") Long idAnime) {
+	@GetMapping ("/{idanime}")
+	public ResponseEntity findCharcterById(@PathVariable(name="idAnime") Long idAnime) {
 		if(idAnime == null) {
 			return ResponseEntity.badRequest().body("cannot find anime avec id null");
 		}
-		AnimeCharacter anime= animeRepository.getOne(idAnime);
-		if(anime == null) {
+		AnimeCharacter character= charcterRepository.getOne(idAnime);
+		if(character == null) {
 			return ResponseEntity.notFound().build();
 		}
-		return ResponseEntity.ok(anime);
+		return ResponseEntity.ok(character);
 	}
+	@PostMapping("/")
+	public ResponseEntity createCharcter (@RequestBody  AnimeCharacter character) {
+		if( character == null) {
+			return ResponseEntity.badRequest().body("cannot create character with empty fields");
+		}
+		return ResponseEntity.ok(charcterRepository.save(character));
+	}
+	
 }
